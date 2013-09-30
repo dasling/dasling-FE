2a3a295ece77bc89f62c97a0a7bfe54403d502ff
Paul-Armand Verhaegen <paularmand.verhaegen@gmail.com>
2013-09-30 21:46:45 +0200

    added db_inserts.sql

---
+++ db/db_inserts.sql
@@ -0,0 +1,24 @@
+-- -----------------------------------------------------
+-- Data for table `perp_v1`.`statuses`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `perp_v1`;
+INSERT INTO `perp_v1`.`statuses` (`status_id`, `description`) VALUES (1, 'Enabled');
+INSERT INTO `perp_v1`.`statuses` (`status_id`, `description`) VALUES (16, 'Disabled');
+INSERT INTO `perp_v1`.`statuses` (`status_id`, `description`) VALUES (100, 'Information missing');
+INSERT INTO `perp_v1`.`statuses` (`status_id`, `description`) VALUES (255, 'Deleted');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `perp_v1`.`organizations`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `perp_v1`;
+INSERT INTO `perp_v1`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (1, 'KU Leuven/CIB', 1);
+INSERT INTO `perp_v1`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (2, 'FMTC', 1);
+INSERT INTO `perp_v1`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (3, 'VUB/COMO', 1);
+
+COMMIT;
+
+
