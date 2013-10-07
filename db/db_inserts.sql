d03374fb5fe54b8aa2c77ec1b4473c25166eac9e
paularmand <paularmand.verhaegen@gmail.com>
2013-10-07 10:07:37 +0000

    Using dasling in insert commands

--- db/db_inserts.sql
+++ db/db_inserts.sql
@@ -2,11 +2,11 @@
 -- Data for table `dasling`.`statuses`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `perp_v1`;
-INSERT INTO `perp_v1`.`statuses` (`status_id`, `description`) VALUES (1, 'Enabled');
-INSERT INTO `perp_v1`.`statuses` (`status_id`, `description`) VALUES (16, 'Disabled');
-INSERT INTO `perp_v1`.`statuses` (`status_id`, `description`) VALUES (100, 'Information missing');
-INSERT INTO `perp_v1`.`statuses` (`status_id`, `description`) VALUES (255, 'Deleted');
+USE `dasling`;
+INSERT INTO `dasling`.`statuses` (`status_id`, `description`) VALUES (1, 'Enabled');
+INSERT INTO `dasling`.`statuses` (`status_id`, `description`) VALUES (16, 'Disabled');
+INSERT INTO `dasling`.`statuses` (`status_id`, `description`) VALUES (100, 'Information missing');
+INSERT INTO `dasling`.`statuses` (`status_id`, `description`) VALUES (255, 'Deleted');
 
 COMMIT;
 
@@ -14,10 +14,10 @@ COMMIT;
 -- Data for table `dasling`.`organizations`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `perp_v1`;
-INSERT INTO `perp_v1`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (1, 'KU Leuven/CIB', 1);
-INSERT INTO `perp_v1`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (2, 'FMTC', 1);
-INSERT INTO `perp_v1`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (3, 'VUB/COMO', 1);
+USE `dasling`;
+INSERT INTO `dasling`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (1, 'KU Leuven/CIB', 1);
+INSERT INTO `dasling`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (2, 'FMTC', 1);
+INSERT INTO `dasling`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (3, 'VUB/COMO', 1);
 
 COMMIT;
 
