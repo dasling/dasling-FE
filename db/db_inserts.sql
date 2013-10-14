aa6f9148c0d7b36d633273c579bc23bc407193ad
paularmand <paularmand.verhaegen@gmail.com>
2013-10-14 15:48:13 +0200

    Removed the DB name

--- db/db_inserts.sql
+++ db/db_inserts.sql
@@ -2,7 +2,6 @@
 -- Data for table `dasling`.`statuses`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `dasling`;
 INSERT INTO `dasling`.`statuses` (`status_id`, `description`) VALUES (1, 'Enabled');
 INSERT INTO `dasling`.`statuses` (`status_id`, `description`) VALUES (16, 'Disabled');
 INSERT INTO `dasling`.`statuses` (`status_id`, `description`) VALUES (100, 'Information missing');
@@ -14,7 +13,6 @@ COMMIT;
 -- Data for table `dasling`.`organizations`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `dasling`;
 INSERT INTO `dasling`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (1, 'KU Leuven/CIB', 1);
 INSERT INTO `dasling`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (2, 'FMTC', 1);
 INSERT INTO `dasling`.`organizations` (`organization_id`, `name`, `status_id`) VALUES (3, 'VUB/COMO', 1);
